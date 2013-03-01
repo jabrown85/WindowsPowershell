25422990216e394dc5dd5a35ab5a2a7638b8113f
Jesse Brown <jabrown85@gmail.com>
2013-03-01 11:48:54 -0600

    fixed home directories with spaces in them...

--- scripts/vcprompt.bat
+++ scripts/vcprompt.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-python %HOMEPATH%\Documents\WindowsPowerShell\scripts\vcprompt.py --format-hg "[%%s:%%b (%%r:%%h)]"  --format-git "[%%s:%%b (%%r)]"
+python "%HOMEPATH%\Documents\WindowsPowerShell\scripts\vcprompt.py" --format-hg "[%%s:%%b (%%r:%%h)]"  --format-git "[%%s:%%b (%%r)]"
